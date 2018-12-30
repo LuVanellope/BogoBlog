4b3aa2672f256725cb6d175ef869f177a512c1d0
Lu <luacosta.fc@gmail.com>
2018-12-30 09:14:20 -0500

    change label from comment to body in c

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.friendly.find(params[:post_id])
-    @comment = @post.comments.create(params[:comment].permit[:comment])
+    @comment = @post.comments.create(params[:comment].permit[:body])
     redirect_to post_path(@post)
   end
 end
