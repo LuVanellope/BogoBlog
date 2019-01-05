7c493b13d01eae7872dd3038f354db704df8be29
Lu <luacosta.fc@gmail.com>
2019-01-05 13:10:25 -0500

    I18n

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   #validates :name, presence: true
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  validates :password, length: { in: 6..20 }
 end
